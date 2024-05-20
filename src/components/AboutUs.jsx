949d06f6e3d922f56528404187af34a48eed1e24
Lizbeth Almonte <lizbethsa1996@gmail.com>
2024-05-19 20:50:30 -0500

    1905

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,12 +3,15 @@ import { FaCircleCheck } from "react-icons/fa6";
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import React, { useEffect } from 'react'
-
+import about from '../components/assets/images/aboutus.jpg';
 
 const AboutUs = () => {
-    useEffect(()=> {
+
+    /*AOS*/
+    useEffect(() => {
         Aos.init();
-    }, )
+    },)
+    /*********************************************/
 
     const [header] = React.useState({
         mainHeader: "Frequently Asked Questions"
@@ -45,12 +48,12 @@ const AboutUs = () => {
     ]);
 
     return (
-        <div className="hero">
+        <div>
 
-            <section className="bg bg-black">
-                <div className="container1">
+            <section className="page__title bg-black">
+                <div className="container">
                     <div className="row">
-                        <div class="col-6 d-flex justify-content-center">
+                        <div class="d-flex justify-content-center">
                             <div className="content__text text-warning text-center" data-aos="zoom-out-down">
                                 <h1 className="display-1 fw-bold">About Us</h1>
                                 <p className="display-5">OUR HISTORY</p>
@@ -60,56 +63,59 @@ const AboutUs = () => {
                 </div>
             </section>
 
-            <section>
-
-                <div className="row container__contact">
-                    <div className="col-md-6 d-flex justify-content-center">
-                        <div className="aboutus__img">
-                            <img src="assets/images/aboutus.jpg" className="rounded" alt="aboutus" />
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="aboutus__info">
-                            <h1 className="aboutus_title">Who We are</h1>
-                            <div className="aboutus__info-p1">
-                                Welcome to Tech Corp, where innovation meets reliability. Founded on the principles of cutting-edge technology and unwavering commitment to excellence, we are your trusted partner for business services and IT solutions.
+            
+                <section className="container">
+                    <div className="content row">
+                        <div className="col-md-6 d-flex">
+                            <div className="aboutus__img" data-aos="flip-left">
+                                <img src={about} alt="about us" width="450px" height="350px"/>
                             </div>
-                            <div className="about__info-p2">
-                                At Tech Corp, we are a team of passionate professionals dedicated to empowering businesses with transformative technology solutions. With a rich history of successful partnerships and a forward-thinking approach, we strive to be at the forefront of the ever-evolving tech landscape.
-                            </div>
-                            <div class="d-grid gap-2 mx-auto">
-                        <button type="button" class="btn btn-outline-dark">CONTACT US</button>
                         </div>
-                        </div>
-                        
-                    </div>
-                </div>
-            </section>
-
-            <section className="question">
-                <div className="container_question">
-                    <div className="question__header">
-                        <div className="common">
-                            <h3 className="heading">{header.mainHeader}</h3>
-                            <div className="commonBorder"></div>
-                        </div>
-
-                        <div className="row">
-                            {state.map((info) => (
-                                
-                                <div className="col-md-6 box">
-                                    <div className="question__box">
-                                        {info.icon}
-                                        <div className="question__box-header">{info.heading}</div>
-                                        <div className="question__box-p">{info.text}</div>
-                                    </div>
+                        <div className="col-md-6">
+                            <div className="aboutus__info mt-0 mb-0" data-aos="flip-right">
+                                <h1 className="aboutus__title text-black" data-aos="fade-right">Who We are</h1>
+                                <div className="about__text fs-6">
+                                <p className="aboutus__info-p1" data-aos="fade-left">
+                                    Welcome to WebMosaic, where innovation meets reliability. Founded on the principles of cutting-edge technology and unwavering commitment to excellence, we are your trusted partner for business services and IT solutions.
+                                </p>
+                                <p className="about__info-p2" data-aos="fade-left">
+                                    At WebMosaic, we are a team of passionate professionals dedicated to empowering businesses with transformative technology solutions. With a rich history of successful partnerships and a forward-thinking approach, we strive to be at the forefront of the ever-evolving tech landscape.
+                                </p>
                                 </div>
-                            ))}
+                                <div class="d-grid gap-2 mx-auto" data-aos="fade-up" data-aos-duration="3000">
+                                    <button type="button" class="btn btn-outline-dark">CONTACT US</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        </div >
+                </section>
+
+                <section className="question text-black">
+                    <div className="container" data-aos="zoom-in-up">
+                        <div className="container_question">
+                            <div className="question__header" data-aos="fade-up-left">
+                                <div className="common">
+                                    <h3 className="heading text-black fs-1">{header.mainHeader}</h3>
+                                    <div className="commonBorder bg-light"></div>
+                                </div>
+
+                                <div className="row">
+                                    {state.map((info) => (
+
+                                        <div className="col-md-6" data-aos="fade-up" data-aos-duration="3000">
+                                            <div className="question__box">
+                                                {info.icon}
+                                                <div className="question__box-header fs-5">{info.heading}</div>
+                                                <div className="question__box-p fs-6 text-black">{info.text}</div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+        </div>
     )
 }
 
--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,20 +1,96 @@
-import React from 'react'
-import { FaFacebookF, FaTwitter, FaInstagram, FaWhatsapp } from "react-icons/fa";
+
+import { FaFacebookF, FaTwitter, FaInstagram, FaWhatsapp, FaHome , FaPhoneVolume, FaMailBulk } from "react-icons/fa";
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import React, { useEffect } from 'react'
 
 
 
 
 const ContactUs = () => {
 
-    return (
+    /*AOS*/
+    useEffect(() => {
+        Aos.init();
+    },)
+    /*********************************************/
+
+    const [header] = React.useState({
+        mainHeader: "TO MAKE REQUESTS FOR FURTHER INFORMATION, CONTACT US"
+    });
+    const [state] = React.useState([
+        {
+            id: 1,
+            icon: <FaHome className="commonIcons" />,
+            heading: "Address:",
+            text:
+                "Your Address 00 00000",
+        },
+        {
+            id: 2,
+            icon: <FaPhoneVolume className="commonIcons" />,
+            heading: "Phone:",
+            text:
+                "321 123456",
+        },
+        {
+            id: 3,
+            icon: <FaMailBulk className="commonIcons" />,
+            heading: "Email:",
+            text:
+                "your@email.com",
+        },
+    ]);
+    return ( 
         <div>
-            <section className="bg bg-warning">
-                <div className="container1">
-                    <div className="row d-flex">
-                        <div className="col-6">
-                            <div className="bg__content d-flex justify-content-center">
-                                <div className="bg__section">
-                                    <ul className="contactCircles bg-warning">
+            <section className=" page__title page__contact">
+                <div className="container">
+                    <div className="row">
+                        <div className=" d-flex justify-content-center">
+                            <div className="content__contact">
+                                <div className="content__text">
+
+                                    <div className="col-6 d-flex">
+                                        <div className="container__text text-black " data-aos="zoom-out-down">
+                                            <h1 className="display-1 fw-bold">Contact Us</h1>
+                                            <p className="display-5">DO YOU NEED HELP?</p>
+                                        </div>
+                                    </div>
+
+                                    
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+            <section className="container">
+                <div className="content row">
+                    <div className="col-md-6">
+                        <div className="contactus__info">
+                        <div className="common__contact">
+                                    <h3 className="title__info text-black fs-2 p-4">{header.mainHeader}</h3>
+                                </div>
+
+                                <div>
+                                    {state.map((info) => (
+
+                                        <div data-aos="fade-up" data-aos-duration="3000">
+                                            <div className="contact__info p-3">
+                                                {info.icon}
+                                                <div className="fs-5">{info.heading}</div>
+                                                <div className="fs-6 text-black">{info.text}</div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+
+                                <ul className="contactCircles">
                                         <li>
                                             <FaFacebookF />
                                         </li>
@@ -28,68 +104,49 @@ const ContactUs = () => {
                                             <FaWhatsapp />
                                         </li>
                                     </ul>
+                        </div>
+                    </div>
 
-                                    <div className="col-6 d-flex align-items-center">
-                                        <div className="container__text text-black ">
-                                            <h1 className="display-1 fw-bold">Contact Us</h1>
-                                            <p className="display-5">DO YOU NEED HELP?</p>
-                                        </div>
-                                    </div>
+                    <div className="container_formulario">
+                        <div className="formulario col-6 m-2 p-4 d-flex content-justify-center">
+                            <form>
+                                <div class="input-box mb-3">
+                                    <input type="text" id="exampleFormControlInput1" required />
+                                    <label for="" class="form-label">Full Name</label>
+                                    <span></span>
                                 </div>
-                            </div>
+                                <div class="input-box mb-3">
+                                    <input type="text" id="exampleFormControlInput1" required />
+                                    <label for="" class="form-label">Last Name</label>
+                                    <span></span>
+                                </div>
+                                <div class="input-box mb-3">
+                                    <input type="email" id="exampleFormControlInput1" required />
+                                    <label for="" class="form-label">Your Email</label>
+                                    <span></span>
+                                </div>
+                                <div class="input-box mb-3">
+                                    <input type="tel" id="exampleFormControlInput1" required />
+                                    <label for="" class="form-label">Phone</label>
+                                    <span></span>
+                                </div>
+                                <div class="input-box mb-3">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                    <label for="exampleFormControlTextarea1" class="form-label"></label>
+                                </div>
+
+                                <div class="d-grid gap-2 mx-auto">
+                                    <button type="button" class="btn btn-outline-warning">SEND</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
+
                 </div>
             </section>
 
-
-            <section className="row">
-            <div className="col-md-6 d-flex content-justify-center">
-                    <div className="contactus__img">
-                        <img src="assets/images/contactus.jpg" alt="contact us" />
-                    </div>
-                </div>
-
-                <div className="container_formulario">
-                <div className="formulario col-6 m-2 p-4 d-flex content-justify-center">
-                    <form>
-                        <div class="input-box mb-3">
-                            <input type="text" id="exampleFormControlInput1" required />
-                            <label for="" class="form-label">Full Name</label>
-                            <span></span>
-                        </div>
-                        <div class="input-box mb-3">
-                            <input type="text" id="exampleFormControlInput1" required />
-                            <label for="" class="form-label">Last Name</label>
-                            <span></span>
-                        </div>
-                        <div class="input-box mb-3">
-                            <input type="email" id="exampleFormControlInput1" required />
-                            <label for="" class="form-label">Your Email</label>
-                            <span></span>
-                        </div>
-                        <div class="input-box mb-3">
-                            <input type="tel" id="exampleFormControlInput1" required />
-                            <label for="" class="form-label">Phone</label>
-                            <span></span>
-                        </div>
-                        <div class="input-box mb-3">
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                            <label for="exampleFormControlTextarea1" class="form-label">Your Message</label>
-                        </div>
-                        
-                        <div class="d-grid gap-2 mx-auto">
-                        <button type="button" class="btn btn-outline-warning">SEND</button>
-                        </div>
-                    </form>
-                </div>
-                </div>
-                
-                
-            </section>
-            
         </div>
-        
+
     )
 }
 
